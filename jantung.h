389c7cd1d21a926c58fef0e80f03722c48734086
yara22aulia <yara22aulia@gmail.com>
2026-05-12 18:13:27 +0700

    menambahkan class jantung pada file header jantung.h

---
+++ jantung.h
@@ -0,0 +1,17 @@
+#ifndef JANTUNG_H
+#define JANTUNG_H
+
+//membuat class jantung di file header jantung.h
+class jantung {
+public:
+    //membuat constructor dan destructor class jantung
+    jantung() {
+        cout << "jantung dibuat\n";
+    }
+
+    ~jantung() {
+        cout << "jantung dihapus\n";
+    }
+};
+
+#endif
